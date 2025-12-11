934b468592eb07fb62e307155a545ba89f11291b
Pedro Gabriel <leo140201@hotmail.com>
2025-12-11 12:51:39 -0300

    change: updated types

--- lua/gruvbox/types.lua
+++ lua/gruvbox/types.lua
@@ -1,25 +1,37 @@
 ---@meta
 
----@alias Background "dark"|"light"
----@alias Contrast "hard"|"medium"|"soft"
----@alias Style "comment"|"operator"|"selection"|"sign"|"string"
+---@alias Background
+---|"dark"
+---|"light"
 
----@class GruvboxCache
----@field inputs GruvboxInputs
----@field groups table<string, vim.api.keyset.highlight>
+---@alias Contrast
+---|"hard"
+---|"medium"
+---|"soft"
+
+---@alias Style
+---|"comment"
+---|"operator"
+---|"selection"
+---|"sign"
+---|"string"
 
 ---@alias GruvboxColors GruvboxLight|GruvboxDark
 
 ---@class GruvboxHighlight: vim.api.keyset.highlight
 ---@field style Style?
 
----@alias GruvboxHighlights table<string, GruvboxHighlight>
+---@alias GruvboxHighlights         table<string, GruvboxHighlight>
 ---@alias GruvboxHighlightsResolved table<string, vim.api.keyset.highlight>
 
 ---@alias GruvboxHighlightsGet fun(colors: GruvboxColors, opts: GruvboxConfig): GruvboxHighlights
 
 ---@class GruvboxInputs
----@field id string
----@field colors GruvboxColors
+---@field id      string
+---@field colors  GruvboxColors
 ---@field plugins table<string, string>
----@field opts GruvboxConfig
+---@field opts    GruvboxConfig
+
+---@class GruvboxCache
+---@field inputs GruvboxInputs
+---@field groups table<string, vim.api.keyset.highlight>
